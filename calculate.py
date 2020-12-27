ee3af2770dabe348b9250e781ee00754fb7cd204
Iván Ubiñas <ivanubigen@gmail.com>
2020-12-27 18:01:56 -0400

    [calculate.py] Add count_technologies_by_year implementation

--- calculate.py
+++ calculate.py
@@ -2,7 +2,18 @@
 import re
 import json
 
-DATA_PATH: str = "data"
+DATA_OUTPUT_PATH: str = "data/result"
+DATA_SOURCE_PATH: str = "data/source"
+TECHNOLOGIES_FILE_PATH: str = f"{DATA_SOURCE_PATH}/technologies.json"
+JOB_POSTS_FILE_PATH: str = f"{DATA_SOURCE_PATH}/data.json"
+TECHNOLOGIES_TYPES: list = [
+    "languages",
+    "frameworks",
+    "clouds",
+    "databases",
+    "clouds",
+    "mobile",
+]
 
 
 def check_presence(technology, text: str) -> bool:
@@ -12,8 +23,8 @@ def check_presence(technology, text: str) -> bool:
 
     if isinstance(technology, list):
         for name_variation in technology:
-            name_variation_escaped = re.escape(name_variation)
-            if re.search(rf"(^|[^a-zA-Z]){name_variation_escaped}([^a-zA-Z]|$)", text):
+            escaped_name_variation = re.escape(name_variation)
+            if re.search(rf"(^|[^a-zA-Z]){escaped_name_variation}([^a-zA-Z]|$)", text):
                 return True
         return False
     return False
@@ -55,16 +66,50 @@ def group_job_posts_by_year(job_posts: list) -> dict:
     return job_posts_by_year
 
 
-if __name__ == "__main__":
-
-    technologies_data: dict = get_data_from_file(f"{DATA_PATH}/technologies.json")
-    job_posts: list = get_data_from_file(f"{DATA_PATH}/data.json")
-    job_posts_by_year: dict = group_job_posts_by_year(job_posts)
+def count_technologies(technologies: list, job_posts: list):
     technologies_count: dict = {}
-
-    for technology in technologies_data["clouds"]:
+    for technology in technologies:
         if isinstance(technology, list):
             technologies_count[technology[0]] = count_occurrences(technology, job_posts)
         else:
             technologies_count[technology] = count_occurrences(technology, job_posts)
-    # print(sort_dict(new_dict, descending=True))
+    return sort_dict(technologies_count, descending=True)
+
+
+def count_technologies_by_year(_job_posts_by_year: dict, _technologies: list):
+    _technologies_count_by_year: dict = {}
+    for year, year_job_posts in _job_posts_by_year.items():
+        _technologies_count_by_year[year] = {}
+        _technologies_count_by_year[year] = count_technologies(
+            _technologies, year_job_posts
+        )
+    return _technologies_count_by_year
+
+
+def save_data_to_file(data: str, file_path: str):
+    with open(file_path, "w") as f:
+        print(data, file=f)
+
+
+if __name__ == "__main__":
+
+    technologies_data: dict = get_data_from_file(TECHNOLOGIES_FILE_PATH)
+    job_posts: list = get_data_from_file(JOB_POSTS_FILE_PATH)
+    job_posts_by_year: dict = group_job_posts_by_year(job_posts)
+    print("Counting technologies, this might take a while...")
+    for technology_type in TECHNOLOGIES_TYPES:
+        technologies_count: dict = count_technologies(
+            technologies_data[technology_type], job_posts
+        )
+        technologies_count_by_year: dict = count_technologies_by_year(
+            job_posts_by_year, technologies_data[technology_type]
+        )
+        save_data_to_file(
+            json.dumps(technologies_count),
+            f"{DATA_OUTPUT_PATH}/{technology_type}_result.json",
+        )
+        save_data_to_file(
+            json.dumps(technologies_count_by_year),
+            f"{DATA_OUTPUT_PATH}/{technology_type}_by_year_result.json",
+        )
+    print(f"Done! Check the results at ./{DATA_OUTPUT_PATH}.")
